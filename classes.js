005a1daec405e896b39212b506745ee7ddbec349
distoneman <distoneman@gmail.com>
2018-11-30 09:53:54 -0700

    updated classes.js

--- classes.js
+++ classes.js
@@ -177,5 +177,29 @@ class ProgressiveManager {
 */
 
 //Code Here
+class Machine{
+  constructor(){
+    
+    this.widgets_made_count = 0
+    this.wear_and_tear_count = 0
+    this.needs_reboot = false
+  }
 
+  makeWidgets(num){
+    this.widgets_made_count += num
+    this.wear_and_tear_count += num /50
+  }
+
+  fixMachine(){
+    this.needs_reboot = true
+  }
+
+  reboot(){
+    return () => {
+      this.wear_and_tear_count -= 10
+      this.needs_reboot = false
+    }
+  }
+
+}
 
